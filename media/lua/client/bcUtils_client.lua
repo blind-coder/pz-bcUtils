1e94889adb09bfe7a340bc714a3fcb32c20ffba4
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-07-07 10:04:44 +0200

    add force reload lua button

---
+++ media/lua/client/bcUtils_client.lua
@@ -0,0 +1,24 @@
+require "bcUtils"
+require "OptionScreens/MainScreen"
+require "ISUI/ISButton"
+
+if not bcUtils then bcUtils = {} end
+
+bcUtils.reloadLua = function() -- {{{
+	getCore():ResetLua(true, "modsChanged") -- Boom!
+end
+-- }}}
+bcUtils.initMainMenu = function() -- {{{
+	bcUtils.forceReloadLuaButton = ISButton:new(MainScreen.instance.width-150, 50, 150, 25, "Forcereload LUA", nil, bcUtils.reloadLua);
+	bcUtils.forceReloadLuaButton.borderColor = {r=1, g=1, b=1, a=0.1};
+	bcUtils.forceReloadLuaButton:ignoreWidthChange();
+	bcUtils.forceReloadLuaButton:ignoreHeightChange();
+	bcUtils.forceReloadLuaButton:setAnchorLeft(false);
+	bcUtils.forceReloadLuaButton:setAnchorRight(true);
+	bcUtils.forceReloadLuaButton:setAnchorTop(true);
+	bcUtils.forceReloadLuaButton:setAnchorBottom(false);
+
+	MainScreen.instance:addChild(bcUtils.forceReloadLuaButton);
+end
+-- }}}
+Events.OnMainMenuEnter.Add(bcUtils.initMainMenu);
